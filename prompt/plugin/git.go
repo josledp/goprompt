1f58486ccf2584b1c6229a8e57ba525bb92d7c69
José Luis Ledesma <joseluis.ledesma@gmail.com>
2017-10-26 09:26:01 +0200

    comment out git2go part...

--- prompt/plugin/git.go
+++ prompt/plugin/git.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/josledp/termcolor"
-	git2go "gopkg.in/libgit2/git2go.v26"
 )
 
 const (
@@ -45,124 +44,129 @@ func (Git) Name() string {
 
 //Load is the load function of the plugin
 func (g *Git) Load(pr Prompter) error {
-	gitpath, err := git2go.Discover(".", false, []string{"/"})
-	if err == nil {
-		repository, err := git2go.OpenRepository(gitpath)
-		if err != nil {
-			return fmt.Errorf("Error opening repository at %s: %v", gitpath, err)
-		}
-		defer repository.Free()
-
-		//Get current tracked & untracked files status
-		statusOpts := git2go.StatusOptions{
-			Flags: git2go.StatusOptIncludeUntracked | git2go.StatusOptRenamesHeadToIndex,
-		}
-		repostate, err := repository.StatusList(&statusOpts)
-		if err != nil {
-			return fmt.Errorf("Error getting repository status at %s: %v", gitpath, err)
-		}
-		defer repostate.Free()
-		n, err := repostate.EntryCount()
-		for i := 0; i < n; i++ {
-			entry, _ := repostate.ByIndex(i)
-			got := false
-			if entry.Status&git2go.StatusCurrent > 0 {
-				got = true
-			}
-			if entry.Status&git2go.StatusIndexNew > 0 {
-				g.staged++
-				got = true
-			}
-			if entry.Status&git2go.StatusIndexModified > 0 {
-				g.staged++
-				got = true
-			}
-			if entry.Status&git2go.StatusIndexDeleted > 0 {
-				g.staged++
-				got = true
-			}
-			if entry.Status&git2go.StatusIndexRenamed > 0 {
-				g.staged++
-				got = true
-			}
-			if entry.Status&git2go.StatusIndexTypeChange > 0 {
-				g.staged++
-				got = true
-			}
-			if entry.Status&git2go.StatusWtNew > 0 {
-				g.untracked++
-				got = true
-			}
-			if entry.Status&git2go.StatusWtModified > 0 {
-				g.changed++
-				got = true
-			}
-			if entry.Status&git2go.StatusWtDeleted > 0 {
-				g.changed++
-				got = true
-			}
-			if entry.Status&git2go.StatusWtTypeChange > 0 {
-				g.changed++
-				got = true
-			}
-			if entry.Status&git2go.StatusWtRenamed > 0 {
-				g.changed++
-				got = true
-			}
-			if entry.Status&git2go.StatusIgnored > 0 {
-				got = true
-			}
-			if entry.Status&git2go.StatusConflicted > 0 {
-				g.conflicted++
-				got = true
-			}
-			if !got {
-				log.Println("Git plugin. Unknown: ", entry.Status)
-			}
-		}
-		//Get current branch name
-		localRef, err := repository.Head()
-		if err != nil {
-			//Probably there are no commits yet. How to know the current branch??
-			g.branch = "No_Commits"
-			return nil
-		}
-		defer localRef.Free()
-
-		localBranch := localRef.Branch()
-		if err != nil {
-			return fmt.Errorf("Error getting local branch: %v", err)
-		}
-
-		if isHead, _ := localBranch.IsHead(); isHead {
-			g.branch = localRef.Shorthand()
-		} else {
-			g.branch = ":" + localRef.Target().String()[:7]
-			g.detached = true
-		}
-
-		remoteRef, err := localBranch.Upstream()
-
+	pwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("unable to determine current pwd: %v", err)
+	}
+	/*
+		gitpath, err := git2go.Discover(".", false, []string{"/"})
 		if err == nil {
-			defer remoteRef.Free()
+			repository, err := git2go.OpenRepository(gitpath)
+			if err != nil {
+				return fmt.Errorf("Error opening repository at %s: %v", gitpath, err)
+			}
+			defer repository.Free()
 
-			g.hasUpstream = true
-			g.fetchIfNeeded(pr)
-
-			if !remoteRef.Target().Equal(localRef.Target()) {
-				g.commitsAhead, g.commitsBehind, err = repository.AheadBehind(localRef.Target(), remoteRef.Target())
-				if err != nil {
-					return fmt.Errorf("Error getting commitsAhead/Behing: %v", err)
+			//Get current tracked & untracked files status
+			statusOpts := git2go.StatusOptions{
+				Flags: git2go.StatusOptIncludeUntracked | git2go.StatusOptRenamesHeadToIndex,
+			}
+			repostate, err := repository.StatusList(&statusOpts)
+			if err != nil {
+				return fmt.Errorf("Error getting repository status at %s: %v", gitpath, err)
+			}
+			defer repostate.Free()
+			n, err := repostate.EntryCount()
+			for i := 0; i < n; i++ {
+				entry, _ := repostate.ByIndex(i)
+				got := false
+				if entry.Status&git2go.StatusCurrent > 0 {
+					got = true
+				}
+				if entry.Status&git2go.StatusIndexNew > 0 {
+					g.staged++
+					got = true
+				}
+				if entry.Status&git2go.StatusIndexModified > 0 {
+					g.staged++
+					got = true
+				}
+				if entry.Status&git2go.StatusIndexDeleted > 0 {
+					g.staged++
+					got = true
+				}
+				if entry.Status&git2go.StatusIndexRenamed > 0 {
+					g.staged++
+					got = true
+				}
+				if entry.Status&git2go.StatusIndexTypeChange > 0 {
+					g.staged++
+					got = true
+				}
+				if entry.Status&git2go.StatusWtNew > 0 {
+					g.untracked++
+					got = true
+				}
+				if entry.Status&git2go.StatusWtModified > 0 {
+					g.changed++
+					got = true
+				}
+				if entry.Status&git2go.StatusWtDeleted > 0 {
+					g.changed++
+					got = true
+				}
+				if entry.Status&git2go.StatusWtTypeChange > 0 {
+					g.changed++
+					got = true
+				}
+				if entry.Status&git2go.StatusWtRenamed > 0 {
+					g.changed++
+					got = true
+				}
+				if entry.Status&git2go.StatusIgnored > 0 {
+					got = true
+				}
+				if entry.Status&git2go.StatusConflicted > 0 {
+					g.conflicted++
+					got = true
+				}
+				if !got {
+					log.Println("Git plugin. Unknown: ", entry.Status)
 				}
 			}
+			//Get current branch name
+			localRef, err := repository.Head()
+			if err != nil {
+				//Probably there are no commits yet. How to know the current branch??
+				g.branch = "No_Commits"
+				return nil
+			}
+			defer localRef.Free()
+
+			localBranch := localRef.Branch()
+			if err != nil {
+				return fmt.Errorf("Error getting local branch: %v", err)
+			}
+
+			if isHead, _ := localBranch.IsHead(); isHead {
+				g.branch = localRef.Shorthand()
+			} else {
+				g.branch = ":" + localRef.Target().String()[:7]
+				g.detached = true
+			}
+
+			remoteRef, err := localBranch.Upstream()
+
+			if err == nil {
+				defer remoteRef.Free()
+
+				g.hasUpstream = true
+				g.fetchIfNeeded(pr)
+
+				if !remoteRef.Target().Equal(localRef.Target()) {
+					g.commitsAhead, g.commitsBehind, err = repository.AheadBehind(localRef.Target(), remoteRef.Target())
+					if err != nil {
+						return fmt.Errorf("Error getting commitsAhead/Behing: %v", err)
+					}
+				}
+			}
+			// only works if libgit >= 0.25
+			repository.Stashes.Foreach(func(i int, m string, o *git2go.Oid) error {
+				g.stashed = i + 1
+				return nil
+			})
 		}
-		// only works if libgit >= 0.25
-		repository.Stashes.Foreach(func(i int, m string, o *git2go.Oid) error {
-			g.stashed = i + 1
-			return nil
-		})
-	}
-	return nil
+		return nil*/
 }
 
 //Get returns the string to use in the prompt
