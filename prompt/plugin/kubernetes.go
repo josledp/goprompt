2bab6fbf7714cee709978b507c52c375c8403e4b
José Luis Ledesma <joseluis.ledesma@gmail.com>
2018-05-07 11:08:11 +0200

    honour KUBECONFIG for kubernetes plugin

--- prompt/plugin/kubernetes.go
+++ prompt/plugin/kubernetes.go
@@ -1,18 +1,38 @@
 package plugin
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"strings"
 
 	"github.com/josledp/termcolor"
+	yaml "gopkg.in/yaml.v2"
 )
 
 //Kubernetes is the plugin struct
 type Kubernetes struct {
-	context string
+	context   string
+	namespace string
+}
+
+type k8sconfig struct {
+	APIVersion     string            `yaml:"apiVersion"`
+	Kind           string            `yaml:"kind"`
+	Preferences    map[string]string `yaml:"preferences"`
+	CurrentContext string            `yaml:"current-context"`
+	Clusters       []struct {
+		Name string `yaml:"name"`
+
+		Cluster map[string]string `yaml:"cluster"`
+	} `yaml:"clusters"`
+	Contexts []struct {
+		Name    string            `yaml:"name"`
+		Context map[string]string `yaml:"context"`
+	} `yaml:"contexts"`
+	Users []struct {
+		Name string                 `yaml:"name"`
+		User map[string]interface{} `yaml:"user"`
+	} `yaml:"users"`
 }
 
 //Name returns the plugin name
@@ -28,19 +48,37 @@ func (Kubernetes) Help() (description string, options map[string]string) {
 
 //Load is the load function of the plugin
 func (k *Kubernetes) Load(Prompter) error {
-	file := os.Getenv("HOME") + string(os.PathSeparator) + ".kube/config"
+	file := os.Getenv("KUBECONFIG")
+	if file == "" {
+		file = os.Getenv("HOME") + string(os.PathSeparator) + ".kube/config"
+	}
 	if _, err := os.Stat(file); err != nil {
 		return nil
 	}
+
 	f, err := os.Open(file)
 	if err != nil {
-		return fmt.Errorf("unable to open ~/.kube/config")
+		return fmt.Errorf("unable to open %s: %v", file, err)
 	}
 	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		if bytes.Contains(scanner.Bytes(), []byte("current-context: ")) {
-			k.context = strings.Replace(string(scanner.Bytes()), "current-context: ", "", 1)
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return fmt.Errorf("unable to read file %s: %v", file, err)
+	}
+	var out k8sconfig
+	err = yaml.Unmarshal(data, &out)
+	if err != nil {
+		return fmt.Errorf("unable to unmarshal file %s: %v", file, err)
+	}
+
+	k.context = out.CurrentContext
+	for _, c := range out.Contexts {
+		if c.Name == k.context {
+			if _, ok := c.Context["namespace"]; ok {
+				k.namespace = c.Context["namespace"]
+			} else {
+				k.namespace = "default"
+			}
 			break
 		}
 	}
@@ -51,7 +89,7 @@ func (k *Kubernetes) Load(Prompter) error {
 //Get returns the string to use in the prompt
 func (k Kubernetes) Get(format func(string, ...termcolor.Mode) string) (string, []termcolor.Mode) {
 	if k.context != "" {
-		return format(fmt.Sprintf("%s", strings.Join(strings.Split(k.context, ".")[0:2], ".")), termcolor.Faint), []termcolor.Mode{termcolor.Faint}
+		return format(fmt.Sprintf("%s(%s)", k.context, k.namespace), termcolor.Faint), []termcolor.Mode{termcolor.Faint}
 	}
 	return "", nil
 }
