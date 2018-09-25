c95d4c080673f06038a7cde1eab6b5302cb879e7
José Luis Ledesma <joseluis.ledesma@gmail.com>
2018-09-25 08:57:06 +0200

    fix aws_test

--- prompt/plugin/aws_test.go
+++ prompt/plugin/aws_test.go
@@ -12,21 +12,22 @@ func TestAws(t *testing.T) {
 
 	os.Setenv("AWS_ROLE", "test:xx-yy-zz")
 	os.Setenv("AWS_SESSION_EXPIRE", "1506345326")
-	expected := "\\[\\033[0m\\]\\[\\033[31m\\]test:zz\\[\\033[0m\\]"
+	expectedPrompt := "\\[\\033[0m\\]\\[\\033[31m\\]test:xx-yy-zz\\[\\033[0m\\]"
+	expectedRole := "test:xx-yy-zz"
 
 	a := &Aws{}
 	a.Load(nil)
 
-	if a.role != "test:zz" {
-		t.Error("Invalid AWS role")
+	if a.role != expectedRole {
+		t.Error("Expected role %s, got %s", expectedRole, a.role)
 	}
 
 	if !(time.Unix(1506345326, int64(0)).Equal(a.expire)) {
-		t.Error("Invalid AWS expire time")
+		t.Error("AWS expire time error. expected %d, got %d", 1506345326, a.expire)
 	}
 	output, _ := a.Get(termcolor.EscapedFormat)
-	if output != expected {
-		t.Errorf("Expected %s\nGot      %s", expected, output)
+	if output != expectedPrompt {
+		t.Errorf("Expected %s\nGot      %s", expectedPrompt, output)
 	}
 
 }
